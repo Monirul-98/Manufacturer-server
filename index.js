f082ef9b7f1c1de54875a9a6eda424a9a78f6856
Monirul-98 <monirulsifat@gmail.com>
2022-05-23 16:42:53 +0600

    Products added to database

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6tpod.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("manufacturer").collection("products");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello From Warehouse!");
 });
