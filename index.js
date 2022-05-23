dba7681f66a80bb1dbeac5f72cc1ccc7ad354c97
Monirul-98 <monirulsifat@gmail.com>
2022-05-23 09:50:54 +0600

    Server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello From Warehouse!");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port ", port);
+});
