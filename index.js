9e1be945fec22cf9a612a4c45377bdd061c6f7b6
Monirul-98 <monirulsifat@gmail.com>
2022-05-26 16:31:09 +0600

    Admin added

--- index.js
+++ index.js
@@ -49,6 +49,24 @@ async function run() {
       const user = await userCollection.find().toArray();
       res.send(user);
     });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJwt, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
